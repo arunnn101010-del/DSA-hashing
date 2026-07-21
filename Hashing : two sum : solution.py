4da44d176e75c727d40ac231a3311798f8576d7f
Arun <arunnn101010@gmail.com>
2026-07-21 22:57:15 +0530

    Update Hashing : two sum : solution.py

--- Hashing : two sum : solution.py
+++ Hashing : two sum : solution.py
@@ -1,1 +1,26 @@
+# Promblem - two sum 
+# Approach - Hash map
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 1 & easy 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int tar) {
+        unordered_map<int, int> m;
+        vector<int> ans;
 
+         for(int i=0; i<arr.size(); i++) {
+            int first = arr[i];
+            int sec = tar - first;
+
+            if(m.find(sec) != m.end()) {
+                ans.push_back(i);
+                ans.push_back(m[sec]);
+                break;
+            }
+
+            m[first] = i; // store key value pairs of i 
+        }   
+
+        return ans;
+    }
+};
