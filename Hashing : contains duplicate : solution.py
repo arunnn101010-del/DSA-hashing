5afd882faac64fd108b41439b74beb25c5ba951e
Arun <arunnn101010@gmail.com>
2026-07-25 23:43:03 +0530

    Update Hashing : contains duplicate : solution.py

--- Hashing : contains duplicate : solution.py
+++ Hashing : contains duplicate : solution.py
@@ -1,1 +1,18 @@
+# Promblem - contains duplicate 
+# Approach - hashing 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 217 & easy 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
 
+        for(int num : nums) {
+            if(s.find(num) != s.end()) {
+                return true;
+            }
+            s.insert(num);
+        }
+        return false;
+    }
+};
