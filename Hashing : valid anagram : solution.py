7290744c21911a992a52f180005818c4844d8075
Arun <arunnn101010@gmail.com>
2026-07-26 23:03:23 +0530

    Update Hashing : valid anagram : solution.py

--- Hashing : valid anagram : solution.py
+++ Hashing : valid anagram : solution.py
@@ -1,1 +1,27 @@
+# Promblem - valid anagram 
+# Approach - hashing 
+# Time and space complexity - 0(n) & 0(1) 
+# Leetcode and diffculty level - 242 & easy 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
 
+        if (s.length() != t.length())
+            return false;
+
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        for (char c : t) {
+
+            freq[c - 'a']--;
+
+            if (freq[c - 'a'] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
