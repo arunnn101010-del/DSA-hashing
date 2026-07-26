c388b37e73960c9fd6b5d6e185a3c8fb882a651e
Arun <arunnn101010@gmail.com>
2026-07-26 23:00:10 +0530

    Update hashing : ransom note : solution.py

--- hashing : ransom note : solution.py
+++ hashing : ransom note : solution.py
@@ -1,1 +1,24 @@
+# Promblem - ransom note 
+# Approach - hashing 
+# Time and space complexity - 0(n+m) & 0(n) 
+# Leetcode and diffculty level - 383 & easy 
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
 
+        vector<int> freq(26,0);
+
+        for(char c:magazine)
+            freq[c-'a']++;
+
+        for(char c:ransomNote){
+
+            freq[c-'a']--;
+
+            if(freq[c-'a']<0)
+                return false;
+        }
+
+        return true;
+    }
+};
