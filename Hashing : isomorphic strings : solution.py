fa8f2e2eb088e36b3c4d25f5bc56a8c49e301835
Arun <arunnn101010@gmail.com>
2026-07-27 13:06:14 +0530

    Create Hashing : isomorphic strings : solution.py

---
+++ Hashing : isomorphic strings : solution.py
@@ -0,0 +1,22 @@
+# Promblem - isomorphic strings 
+# Approach - hashing 
+# Time and space complexity - 0(n) 0(1) 
+# Leetcode and diffculty level - 205 & easy 
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> m1,m2;
+
+        for(int i=0; i<s.size(); i++) {
+            char c1 = s[i];
+            char c2 = t[i];
+
+            if(m1.count(c1) && m1[c1] != c2) return false;
+            if(m2.count(c2) && m2[c2] != c1) return false;
+
+            m1[c1] = c2;
+            m2[c2] = c1;
+        }
+        return true;
+    }
+};
