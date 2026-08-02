f8d4931093ef8c155acce628e27463d845230760
Arun <arunnn101010@gmail.com>
2026-08-02 16:27:10 +0530

    Update Hashing : subarray sum equals to k : solution.py

--- Hashing : subarray sum equals to k : solution.py
+++ Hashing : subarray sum equals to k : solution.py
@@ -1,1 +1,25 @@
+# Promblem - subarray sum equals to k 
+# Approach - prefixSum + hashing 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 560 & medium 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
 
+        mp[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for(int num : nums) {
+            prefixSum += num;
+
+            if(mp.count(prefixSum - k)) {
+                count += mp[prefixSum - k];
+            }
+            mp[prefixSum]++;
+        }
+        return count;
+    }
+};
