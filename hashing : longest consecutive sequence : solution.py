21fca1bf0ce385d595c5dfdf0a07611938556e35
Arun <arunnn101010@gmail.com>
2026-08-02 21:19:57 +0530

    Update hashing : longest consecutive sequence : solution.py

--- hashing : longest consecutive sequence : solution.py
+++ hashing : longest consecutive sequence : solution.py
@@ -1,1 +1,28 @@
+# Promblem - longest consecutive sequence 
+# Approach - hash set 
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 128 & medium 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st(nums.begin(),nums.end());
 
+        int longest = 0;
+
+        for(int num : st) {
+
+            if(!st.count(num-1)) {
+
+                int current = num;
+                int length = 1;
+            
+                while(st.count(current + 1)) {
+                    current++;
+                    length++;
+                }
+                longest = max(longest,length);
+            }
+        }
+        return longest;
+    }
+};
